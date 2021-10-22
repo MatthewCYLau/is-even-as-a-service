c4b68e6cfd5e68e8642c45833fc25b3a67842f9e
Matthew Lau <lau.cy.matthew@gmail.com>
2021-10-22 23:17:13 +0100

    chore: removed annotations

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -31,18 +31,6 @@ export default () => {
     ]);
 
     return (
-        /**
-         * The entire application is wrapped in the Material UI theme
-         * provider. All components can and should be styled using the
-         * theme object. In particular, spacing (i.e. margins and
-         * padding) should be defined using the 'theme.spacing' method
-         * and colours should be accessed via the 'theme.palette' object.
-         * By always using the theme object to style components, changes
-         * only need to be made in one place (config/Theme.js) and the
-         * entire application will have its appearance updated consistantly.
-         *
-         * See: https://material-ui.com/styles/api/#themeprovider
-         */
         <ThemeProvider theme={theme}>
             <App />
         </ThemeProvider>
