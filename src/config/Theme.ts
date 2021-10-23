7c2c05ea05e87fb22c9849b7e82d40ce0bddf72b
Matthew Lau <lau.cy.matthew@gmail.com>
2021-10-23 15:44:32 +0100

    refactor: removed eslint config

--- src/config/Theme.ts
+++ src/config/Theme.ts
@@ -1,31 +1,33 @@
-import createPalette from '@material-ui/core/styles/createPalette';
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-import responsiveFontSizes from '@material-ui/core/styles/responsiveFontSizes';
+import createPalette from "@material-ui/core/styles/createPalette";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import responsiveFontSizes from "@material-ui/core/styles/responsiveFontSizes";
 
-export default (darkMode: boolean) => {
-    const palette = createPalette({
-        primary: {
-            main: '#FF7E39',
-            light: '#F9D5A7',
-            contrastText: '#fff'
-        },
-        secondary: {
-            main: '#90AACB',
-            light: '#FEF1E6',
-        },
-        text: {
-            secondary: 'rgba(0, 0, 0, 0.56)'
-        },
-        type: darkMode ? 'dark' : 'light'
-    });
+const Theme = (darkMode: boolean) => {
+  const palette = createPalette({
+    primary: {
+      main: "#FF7E39",
+      light: "#F9D5A7",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#90AACB",
+      light: "#FEF1E6",
+    },
+    text: {
+      secondary: "rgba(0, 0, 0, 0.56)",
+    },
+    type: darkMode ? "dark" : "light",
+  });
 
-    const theme = createMuiTheme({ palette });
-    theme.overrides = {
-        MuiLink: {
-            root: {
-                fontWeight: 500
-            }
-        }
-    };
-    return responsiveFontSizes(theme);
+  const theme = createMuiTheme({ palette });
+  theme.overrides = {
+    MuiLink: {
+      root: {
+        fontWeight: 500,
+      },
+    },
+  };
+  return responsiveFontSizes(theme);
 };
+
+export default Theme;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,91 +1,80 @@
-import React from 'react';
-import {
-    Container,
-    Grid,
-    Link,
-    Typography,
-} from '@material-ui/core';
-import {
-    Formik,
-    Form,
-    Field,
-  } from 'formik';
+import React from "react";
+import { Container, Grid, Link, Typography } from "@material-ui/core";
+import { Formik, Form, Field } from "formik";
 // import { useTypedSelector } from '../../hooks/useTypedSelector';
-import { useActions } from '../../hooks/useActions';
-import sampleImage from '../../assets/calculator.png';
-import useStyles from './HomePage.style';
+import { useActions } from "../../hooks/useActions";
+import sampleImage from "../../assets/calculator.png";
+import useStyles from "./HomePage.style";
 
 type Section = {
-    key: string;
-    html: React.ReactNode;
+  key: string;
+  html: React.ReactNode;
 };
 
 interface FormValues {
-    inputNumber: number;
+  inputNumber: number;
 }
 
 const HomePage = () => {
-    const styles = useStyles();
-    const { calculateIsEven } = useActions();
+  const styles = useStyles();
+  const { calculateIsEven } = useActions();
 
-    const sections: Section[] = [
-        {
-            key: 'welcome',
-            html: (
-                <Typography>
-                    {'Is-Even As A Service - because '}
-                    <Link
-                        color="secondary"
-                        href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Remainder operator
-                    </Link>
-                    {' is difficult to comprehend '}
-                </Typography>
-            )
-        }
-    ];
+  const sections: Section[] = [
+    {
+      key: "welcome",
+      html: (
+        <Typography>
+          {"Is-Even As A Service - because "}
+          <Link
+            color="secondary"
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Remainder operator
+          </Link>
+          {" is difficult to comprehend "}
+        </Typography>
+      ),
+    },
+  ];
 
-    const initialValues: FormValues = { inputNumber: 0 };
+  const initialValues: FormValues = { inputNumber: 0 };
 
-    return (
-        <Container component="main" maxWidth="lg" className={styles.root}>
-            <div className={styles.content}>
-                <img className={styles.image} src={sampleImage} alt="Sample" />
-                <Grid container spacing={3}>
-                    {
-                        sections.map(({ html, key }) => (
-                            <Grid
-                                key={key}
-                                item
-                                xs={12}
-                                sm={6}
-                                md={12}
-                                className={styles.paragraph}
-                            >
-                                {html}
-                            </Grid>
-                        ))
-                    }
-                </Grid>
-                <Formik
-                    initialValues={initialValues}
-                    onSubmit={(values, actions) => {
-                        calculateIsEven(values.inputNumber);
-                        actions.setSubmitting(false);
-                    }}
-                >
-                    <Form>
-                    <label htmlFor="inputNumber">Is this number even?</label>
-                    <Field id="inputNumber" name="inputNumber" placeholder="0" />
-                    <button type="submit">Submit</button>
-                    </Form>
-                </Formik>
-            </div>
-        </Container>
-    );
+  return (
+    <Container component="main" maxWidth="lg" className={styles.root}>
+      <div className={styles.content}>
+        <img className={styles.image} src={sampleImage} alt="Sample" />
+        <Grid container spacing={3}>
+          {sections.map(({ html, key }) => (
+            <Grid
+              key={key}
+              item
+              xs={12}
+              sm={6}
+              md={12}
+              className={styles.paragraph}
+            >
+              {html}
+            </Grid>
+          ))}
+        </Grid>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={(values, actions) => {
+            calculateIsEven(values.inputNumber);
+            actions.setSubmitting(false);
+          }}
+        >
+          <Form>
+            <label htmlFor="inputNumber">Is this number even?</label>
+            <Field id="inputNumber" name="inputNumber" placeholder="0" />
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
