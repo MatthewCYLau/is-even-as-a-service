add492c79c28c095023397bb084c51bcad7cc8fd
Matthew Lau <lau.cy.matthew@gmail.com>
2021-11-06 17:05:24 +0000

    feat: added dark mode toggle

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { useMediaQuery } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { store } from "../../store";
 import useStyles from "./App.style";
@@ -10,14 +9,22 @@ import Routes from "../../config/Routes";
 import Header from "../Header";
 import HomePage from "../../pages/HomePage";
 
-const App = () => {
+export const ColorModeContext = React.createContext({
+  toggleColorMode: () => {},
+});
+
+type Props = {
+  mode: string;
+};
+
+const App: React.FunctionComponent<Props> = ({ mode }) => {
   const styles = useStyles();
 
   return (
     <Provider store={store}>
       <Router>
         <div className={styles.root}>
-          <Header />
+          <Header mode={mode} />
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route component={Routes} />
@@ -29,16 +36,24 @@ const App = () => {
 };
 
 const AppWithTheme = () => {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const theme = React.useMemo(
-    () => createTheme(prefersDarkMode),
-    [prefersDarkMode]
+  const [mode, setMode] = React.useState("light");
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
   );
 
+  const theme = React.useMemo(() => createTheme(mode), [mode]);
+
   return (
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <App mode={mode} />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 };
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ColorModeContext } from "../App/App";
 import { AppBar, Button, Toolbar, Typography } from "@material-ui/core";
+import IconButton from "@mui/material/IconButton";
 import useStyles from "./Header.style";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 
-const Header: React.FunctionComponent = () => {
+interface HeaderProps {
+  mode: string;
+}
+
+const Header: React.FunctionComponent<HeaderProps> = ({ mode }) => {
   const styles = useStyles();
+  const colorMode = React.useContext(ColorModeContext);
+
   return (
     <AppBar component="header" position="static">
       <Toolbar>
@@ -20,6 +30,13 @@ const Header: React.FunctionComponent = () => {
         >
           Home
         </Button>
+        <IconButton
+          sx={{ ml: 1 }}
+          onClick={colorMode.toggleColorMode}
+          color="inherit"
+        >
+          {mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
